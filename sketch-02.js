28b7cb7cc9100992470e37fe3cce70f32832f593
Lafaiete do Vale <lafa.vale@gmail.com>
2023-05-07 23:36:40 -0300

    Clock pointers

--- sketch-02.js
+++ sketch-02.js
@@ -15,19 +15,32 @@ const sketch = () => {
 
     context.fillStyle = "black";
 
-    const x = width * 0.5;
-    const y = height * 0.5;
-    const w = width * 0.3;
-    const h = height * 0.3;
+    const cx = width * 0.5;
+    const cy = height * 0.5;
 
-    context.save();
-    context.translate(x, y);
-    context.rotate(degToRad(45));
+    const w = width * 0.01;
+    const h = height * 0.1;
+    let x, y;
 
-    context.beginPath();
-    context.rect(-w * 0.5, -h * 0.5, w, h);
-    context.fill();
-    context.restore();
+    const num = 12;
+    const radius = width * 0.3;
+
+    for (let i = 0; i < num; i++) {
+      const slice = degToRad(360 / num);
+      const angle = slice * i;
+
+      x = cx + radius * Math.sin(angle);
+      y = cy + radius * Math.cos(angle);
+
+      context.save();
+      context.translate(x, y);
+      context.rotate(-angle);
+
+      context.beginPath();
+      context.rect(-w * 0.5, -h * 0.5, w, h);
+      context.fill();
+      context.restore();
+    }
   };
 };
 
